96a02940ec16f903e2cb4a0bbeada16beb2764a7
Timothy Joseph Busch <timbusch1@gmail.com>
2019-06-24 09:38:45 -0700

    git initialize

---
+++ src/main/java/com/pawpals/pawpals/PawpalsApplication.java
@@ -0,0 +1,13 @@
+package com.pawpals.pawpals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawpalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawpalsApplication.class, args);
+	}
+
+}
