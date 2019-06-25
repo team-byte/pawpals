9ac0300a729d1432bd3d2b9d444a2417e778a128
Peter Tynan <pettynan@amazon.com>
2019-06-25 13:19:03 -0700

    Review controllers set up according to how I think the models passed in should work

--- src/main/java/com/pawpals/pawpals/controllers/ReviewController.java
+++ src/main/java/com/pawpals/pawpals/controllers/ReviewController.java
@@ -1,9 +1,9 @@
 package com.pawpals.pawpals.controllers;
 
-import com.pawpals.pawpals.models.AppUserRepository;
-import com.pawpals.pawpals.models.PetRepository;
+import com.pawpals.pawpals.models.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -17,10 +17,39 @@ public class ReviewController {
     @Autowired
     PetRepository petRepository;
 
+    @Autowired
+    ReviewRepository reviewRepository;
+
     @PostMapping("/writenewreview")
     public RedirectView writeeNewReview(Principal p, String rating, String body, String target) {
 
 
         return new RedirectView("/");
+    }
+
+    @PostMapping("/users/{targetId}/review")
+    public RedirectView addUserReview(@PathVariable long targetId, Principal p, String rating, String body) {
+        AppUser targetUser = appUserRepository.findById(targetId).get();
+        AppUser reviewAuthor = appUserRepository.findByUsername(p.getName());
+
+        Review review = new Review();
+
+        Review.UserReview userReview = review.new UserReview(Integer.parseInt(rating), body, reviewAuthor, targetUser);
+        reviewRepository.save(userReview);
+
+        return new RedirectView("/users/" + targetId);
+    }
+
+    @PostMapping("/pets/{targetId}/review")
+    public RedirectView addPetReview(@PathVariable long targetId, Principal p, String rating, String body) {
+        Pet targetPet = petRepository.findById(targetId).get();
+        AppUser reviewAuthor = appUserRepository.findByUsername(p.getName());
+
+        Review review = new Review();
+
+        Review.PetReview petReview = review.new PetReview(Integer.parseInt(rating), body, reviewAuthor, targetPet);
+        reviewRepository.save(petReview);
+
+        return new RedirectView("/pets/" + targetId);
     }
 }
