d3ac89440982db9db5d788b0c4a0fb67fcdced7b
Chai.Narukulla <chaitanyanarukulla@gmail.com>
2019-06-27 11:37:29 -0700

    Updated tests

--- src/test/java/com/pawpals/pawpals/controllers/UserControllerTest.java
+++ src/test/java/com/pawpals/pawpals/controllers/UserControllerTest.java
@@ -2,6 +2,7 @@ package com.pawpals.pawpals.controllers;
 
 import com.pawpals.pawpals.models.AppUser;
 import com.pawpals.pawpals.models.AppUserRepository;
+import com.pawpals.pawpals.models.Pet;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -31,11 +32,10 @@ public class UserControllerTest {
     UserController UserController;
 
     @Autowired
+    PetController PetController;
+
+    @Autowired
     MockMvc mockMvc;
-
-//    @Autowired
-//    private TestEntityManager entityManager;
-
 
     @Autowired
     private AppUserRepository repository;
@@ -44,6 +44,78 @@ public class UserControllerTest {
     public void test_ControllerIsAutowired() {
         assertNotNull(UserController);
     }
+
+    @Test
+    public void test_Pet_ControllerIsAutowired() {
+        assertNotNull(PetController);
+    }
+
+    @Test
+    public void test_AppUser_constructor_username() {
+        AppUser classUnderTest = new AppUser("chai", "password","chai","passwerd","289900223");
+        assertEquals("should return name chai","chai",classUnderTest.getUsername());
+    }
+
+    @Test
+    public void test_AppUser_constructor_lname() {
+        AppUser classUnderTest = new AppUser("chai", "Test","Naru","passwerd","289900223");
+        assertEquals("should return name Naru","Naru",classUnderTest.getLastName());
+    }
+
+    @Test
+    public void test_AppUser_constructor_fname() {
+        AppUser classUnderTest = new AppUser("chai", "chaiT","Naru","passwerd","289900223");
+        assertEquals("should return name chaiT","chaiT",classUnderTest.getFirstName());
+    }
+
+    @Test
+    public void test_AppUser_constructor_Phonenumber() {
+        AppUser classUnderTest = new AppUser("chai", "chaiT","Naru","passwerd","289900223");
+        assertEquals("should return name 289900223","289900223",classUnderTest.getPhoneNumber());
+    }
+
+    @Test
+    public void test_Pet_constructor_name(){
+        Pet classUndertest = new Pet("Demi","Dog","Bc",4,"good dog",4,"small","dig.jpg",4,new AppUser());
+        assertEquals("should return name Demi","Demi",classUndertest.getName());
+    }
+
+    @Test
+    public void test_Pet_constructor_breed(){
+        Pet classUndertest = new Pet("Demi","Dog","Bc",4,"good dog",4,"small","dig.jpg",4,new AppUser());
+        assertEquals("should return breed Dog","Bc",classUndertest.getBreed());
+    }
+
+    @Test
+    public void test_Pet_constructor_bio(){
+        Pet classUndertest = new Pet("Demi","Dog","Bc",4,"good dog",4,"small","dig.jpg",4,new AppUser());
+        assertEquals("should return Bio good dog","good dog",classUndertest.getBio());
+    }
+
+    @Test
+    public void test_Pet_constructor_acitivity(){
+        Pet classUndertest = new Pet("Demi","Dog","Bc",4,"good dog",4,"small","dig.jpg",4,new AppUser());
+        assertEquals("should return activity good dog",4,classUndertest.getActivity());
+    }
+
+    @Test
+    public void test_Pet_constructor_size(){
+        Pet classUndertest = new Pet("Demi","Dog","Bc",4,"good dog",4,"small","dig.jpg",4,new AppUser());
+        assertEquals("should return size small","small",classUndertest.getSize());
+    }
+
+    @Test
+    public void test_Pet_constructor_ImgUrl(){
+        Pet classUndertest = new Pet("Demi","Dog","Bc",4,"good dog",4,"small","dig.jpg",4,new AppUser());
+        assertEquals("should return img dig.jpg","dig.jpg",classUndertest.getImgUrl());
+    }
+
+    @Test
+    public void test_Pet_constructor_rating(){
+        Pet classUndertest = new Pet("Demi","Dog","Bc",4,"good dog",4,"small","dig.jpg",4,new AppUser());
+        assertEquals("should return rating",4,classUndertest.getRating());
+    }
+
 
     @Test
     public void test_Request_login() throws Exception {
@@ -59,17 +131,15 @@ public class UserControllerTest {
         mockMvc.perform(get("/signup")).andExpect(content().string(containsString("Username")));
     }
 
+
+    @Test
+    public void test_Request_Aboutus() throws Exception {
+        mockMvc.perform(get("/aboutPawpals")).andExpect(content().string(containsString("")));
+    }
+
     @Test
     public void test_Request_createnewuser() throws Exception {
         mockMvc.perform(get("/createnewuser")).andExpect(content().string(containsString("")));
     }
-
-
-//    @Test
-//    public void testExample() throws Exception {
-//        this.entityManager.persist(new AppUser("ChaiT", "Chai", "Naru", "password","2066788779"));
-//        AppUser user = this.repository.findByUsername("ChaiT");
-//        assertTrue(user.getFirstName().equals("Chai"));
-//    }
 
 }
